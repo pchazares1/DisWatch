9ea4ffb90d4a9ec72a03f36fda33a1267dc680c5
Phillip Chazares <pchazares@ggc.edu>
2020-02-01 14:09:58 -0500

    added google maps node_mod and Map Component

---
+++ dis-watch/src/components/MapContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapsStyles = {
+    width: '100%',
+    height: '100%'
+};
+
+export class MapContainer extends React.Component{
+    render() {
+        return (
+            <Map 
+                google={this.props.google}
+                zoom={8}
+                style={mapsStyles}
+                initialCenter={{ lat: 47.44, lng: -122.176}}
+            />
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyBDEllUUJas4n_DkmMPGbxIfAC8b9q2UU4'
+})(MapContainer);
