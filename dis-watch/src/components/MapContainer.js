7a923fbd5189a16c861cce1a36f2228f8f7ab913
ViNguyen15 <fluffywin@gmail.com>
2020-02-01 17:39:16 -0500

    fixing errors

--- dis-watch/src/components/MapContainer.js
+++ dis-watch/src/components/MapContainer.js
@@ -1,24 +1,24 @@
-// import React from 'react';
-// import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-// const mapsStyles = {
-//     width: '99%',
-//     height: '90%'
-// };
+const mapsStyles = {
+    width: '99%',
+    height: '90%'
+};
 
-// export class MapContainer extends React.Component{
-//     render() {
-//         return (
-//             <Map 
-//                 google={this.props.google}
-//                 zoom={8}
-//                 style={mapsStyles}
-//                 initialCenter={{ lat: 47.44, lng: -122.176}}
-//             />
-//         );
-//     }
-// }
+export class MapContainer extends React.Component{
+    render() {
+        return (
+            <Map 
+                google={this.props.google}
+                zoom={8}
+                style={mapsStyles}
+                initialCenter={{ lat: 47.44, lng: -122.176}}
+            />
+        );
+    }
+}
 
-// export default GoogleApiWrapper({
-//     apiKey: 'AIzaSyBDEllUUJas4n_DkmMPGbxIfAC8b9q2UU4'
-// })(MapContainer);
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyBDEllUUJas4n_DkmMPGbxIfAC8b9q2UU4'
+})(MapContainer);
