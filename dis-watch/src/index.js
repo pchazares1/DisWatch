c70ccaef7298c48234024664f70dc39310252053
zidrees <zidrees@ggc.edu>
2020-02-01 16:14:23 -0500

    Added FireBase to index.js

--- dis-watch/src/index.js
+++ dis-watch/src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import  * as firebase from'firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+var config={
+    apiKey: "AIzaSyBDEllUUJas4n_DkmMPGbxIfAC8b9q2UU4",
+    authDomain: "diswatch.firebaseapp.com",
+    databaseURL: "https://diswatch.firebaseio.com",
+    projectId: "diswatch",
+    storageBucket: "diswatch.appspot.com",
+    messagingSenderId: "6266907700",
+    appId: "1:6266907700:web:57986d7218d6c3b5e7ae61",
+    measurementId: "G-895E1JRDLB"
+}
+firebase.initializeApp(config);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
