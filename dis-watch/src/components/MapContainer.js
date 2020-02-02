1b5e72f45b2573e65cc981da56bbb6115b098be2
Phillip Chazares <pchazares@ggc.edu>
2020-02-01 23:20:02 -0500

    Update maps, markers, and title

--- dis-watch/src/components/MapContainer.js
+++ dis-watch/src/components/MapContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const mapsStyles = {
     width: '99%',
@@ -9,16 +9,23 @@ const mapsStyles = {
 export class MapContainer extends React.Component{
     render() {
         return (
-            <Map 
+            <div>
+                <Map 
                 google={this.props.google}
                 zoom={8}
                 style={mapsStyles}
-                initialCenter={{ lat: 47.44, lng: -122.176}}
-            />
+                > 
+                <Marker onClick={this.onMarkerClick}
+                    name={'Current location'} />
+
+                 </Map>
+
+            </div>
+            
         );
     }
 }
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyBDEllUUJas4n_DkmMPGbxIfAC8b9q2UU4'
+    apiKey: 'AIzaSyDUf6UPWOTW1fmiT_N2fbLZWloXk7fhjpk'
 })(MapContainer);
---
+++ dis-watch/src/components/Marker.js
@@ -0,0 +1,1 @@
+import Marker from 'google-maps-react';  
